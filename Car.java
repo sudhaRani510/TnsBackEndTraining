02f99164bad526df2b8138c77beb474ec9f53690
sudha <sudharanilingampally@gmail.com>
2025-11-09 22:48:36 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,9 @@
+public class Car{
+    int yearOfManufacture;
+    String color;
+    String model;
+    String type;
+    void startEngine(){
+        System.out.println("Engine started");
+    }
+}
