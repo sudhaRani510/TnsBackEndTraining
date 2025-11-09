e138841712604227880719a9247c9f7aea3d879b
sudha <sudharanilingampally@gmail.com>
2025-11-09 22:49:00 +0530

    Add files via upload

---
+++ MainCar.java
@@ -0,0 +1,10 @@
+public class MainCar{
+    public static void main(String[] args){
+        Car c=new Car();
+        c.yearOfManufacture=1999;
+        c.color="red";
+        c.model="shiftdesire";
+        c.type="diesel";
+        c.startEngine();
+    }
+}
